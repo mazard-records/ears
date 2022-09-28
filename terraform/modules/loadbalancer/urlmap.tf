97d02523e5ff10eb6b3a3fe7f874c8435911c0db
Félix Voituret <fvoituret@deezer.com>
2022-09-28 15:04:10 +0200

    🐛 fix urlmap

--- terraform/modules/loadbalancer/urlmap.tf
+++ terraform/modules/loadbalancer/urlmap.tf
@@ -1,7 +1,6 @@
 resource "google_compute_url_map" "primary" {
   name        = format(module.naming.url_map, "primary")
   description = "Global URL map for service routing"
-  default_service = google_compute_backend_service.home.id
 
   host_rule {
     hosts        = var.functions.domains
@@ -10,7 +9,6 @@ resource "google_compute_url_map" "primary" {
 
   path_matcher {
     name = "functions"
-    default_service = google_compute_backend_service.home.id
 
     dynamic "path_rule" {
       for_each = var.functions.targets
