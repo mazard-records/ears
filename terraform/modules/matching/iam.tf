96313457295e3323376231e33ae11c49a0c32fea
Félix Voituret <fvoituret@deezer.com>
2022-09-29 22:19:53 +0200

    🐛 add missing project_id

--- terraform/modules/matching/iam.tf
+++ terraform/modules/matching/iam.tf
@@ -8,7 +8,8 @@ resource "google_service_account" "matching" {
 
 resource "google_project_iam_binding" "matching_log" {
   for_each = toset(var.producers)
-
+  
+  project = module.naming.project_id
   role    = "roles/logging.logWriter"
   members = [
     "serviceAccount:${google_service_account.matching[each.key].email}"
