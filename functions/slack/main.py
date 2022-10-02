82626208ac873ae4c986a2b0c78233d8a010f4df
Félix Voituret <fvoituret@deezer.com>
2022-10-02 16:29:13 +0200

    🐛 fix function CI

--- functions/slack/main.py
+++ functions/slack/main.py
@@ -1,94 +1,81 @@
 import json
-import logging
 
-from functools import lru_cache
-from typing import Callable, Optional
-from urllib.parse import urlparse
+from typing import Any
 
+from ears.messaging import pydantic_model_from_event
+from ears.models import TrackMatching
+from ears.types import Event
 from flask import Request, Response, jsonify
-from google.cloud.logging import Client as LoggingClient
-from httpx import post
 from pydantic import AnyHttpUrl, BaseSettings, Field
 from slackette import (
+    Actions,
     BlockInteraction,
-    InteractionDeleteResponse,
+    Blocks,
+    Button,
+    Divider,
+    Image,
+    Markdown,
+    PlainText,
+    Section,
+    Style,
     SlackWebhook,
-    verify_slack_signature
+    verify_slack_signature,
 )
 
-from matching import (
-    MatchingTrack,
-    MatchingTrackNotification,
-    on_matching_action,
-)
 
-client = LoggingClient()
-client.setup_logging(log_level=logging.DEBUG)
-
-
-class _SlackSettings(BaseSettings):
+class SlackSettings(BaseSettings):
     signing: str = Field(..., env="SLACK_SIGNING_KEY")
     version: str = Field("v0", env="SLACK_VERSION")
     webhook: AnyHttpUrl = Field(..., env="SLACK_WEBHOOK")
 
+    @classmethod
+    def signing_secret_provider(cls) -> str:
+        return cls().signing
+ 
 
-class _InteractionRouter(object):
-    """ Router for interactive callback. """
-
-    def __init__(self) -> None:
-        self._domain_handlers = {}
-
-    def add_domain_handler(
-        self,
-        domain: str,
-        handler: Callable[[str], Optional[str]],
-    ) -> None:
-        self._domain_handlers[domain] = handler
-
-    def run(self, interaction: BlockInteraction) -> None:
-        for action in interaction.actions:
-            url = urlparse(action.value)
-            if url.scheme != "ears":
-                raise ValueError(f"Invalid url scheme {url.scheme}")
-            if url.netloc not in self._domain_handlers:
-                raise ValueError(f"Invalid domain {url.scheme}")
-            self._domain_handlers[url.netloc](url.path)
-            response = post(
-                interaction.response_url,
-                json=InteractionDeleteResponse().dict(),
-            )
-            response.raise_for_status()
-
-
-@lru_cache(maxsize=1)
-def SlackSettings() -> _SlackSettings:
-    return _SlackSettings()
-
-
-@lru_cache(maxsize=1)
-def InteractionRouter() -> None:
-    router = _InteractionRouter()
-    router.add_domain_handler("matching", on_matching_action)
-    return router
-
-
-def signing_secret_provider() -> str:
-    return SlackSettings().signing
-
-
-def on_matching_event(request: Request) -> Response:
+def TrackMatchingNotification(matching: TrackMatching) -> Blocks:
     """
-    HTTP endpoint that publishes a Slack interactive
-    notification when a track matching is received.
+    Build a rich interactive Slack notification using BlockKit
+    to display the provided matched track entity.
     """
-    track = MatchingTrack(**request.get_json())
-    notification = MatchingTrackNotification(track)
-    webhook = SlackWebhook(SlackSettings().webhook)
-    webhook(notification)
+    return Blocks(blocks=[
+        Divider(),
+        Section(
+            text=Markdown(
+                text=(
+                    f"{track.to_markdown_link()}\n"
+                    f"*Release:*\n{matching.metadata.album}\n"
+                    f"*Provider:*\n{matching.destination.provider}"
+                )
+            ),
+            accessory=Image(image_url=matching.metadata.cover),
+        ),
+        Actions(
+            elements=[
+                Button(
+                    style=Style.primary,
+                    text=PlainText(text="Approve"),
+                    value=matching.to_uri("validate")
+                ),
+                Button(
+                    style=Style.danger,
+                    text=PlainText(text="Deny"),
+                    value=matching.to_uri("invalidate")
+                ),
+            ]
+        )
+    ])
+
+
+@verify_slack_signature(signing_secret=SlackSettings.signing_secret_provider)
+def on_command_webhook(request: Request) -> Response:
+    """
+    HTTP endpoint that provides command interactivity.
+    """
     return jsonify(None)
 
 
-@verify_slack_signature(signing_secret=signing_secret_provider)
+@verify_slack_signature(signing_secret=SlackSettings.signing_secret_provider)
 def on_interactive_webhook(request: Request) -> Response:
     """
     HTTP endpoint that acknowledge user feedback from Slack.
@@ -96,14 +83,15 @@ def on_interactive_webhook(request: Request) -> Response:
     payload = request.form.get("payload", {})
     decoded = json.loads(payload)
     interaction = BlockInteraction(**decoded)
-    router = InteractionRouter()
-    router.run(interaction)
+    # TODO: refactor this using protocol abstraction ?
+    # router = InteractionRouter()
+    # router.run(interaction)
     return jsonify(None)
 
 
-@verify_slack_signature(signing_secret=signing_secret_provider)
-def on_command_webhook(request: Request) -> Response:
-    """
-    HTTP endpoint that provides command interactivity.
-    """
-    return jsonify(None)
+def on_matching_event(event: Event, _: Any) -> None:
+    settings = SlackSettings()
+    matching = pydantic_model_from_event(TrackMatching, event)
+    notification = TrackMatchingNotification(matching)
+    webhook = SlackWebhook(settings.webhook)
+    webhook(notification)
